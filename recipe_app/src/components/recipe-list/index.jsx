170c1b37fb91a668c4d875165f830b62b10eb5b0
Pradhan-G <abhishekpradhan12.10.00@gmail.com>
2024-06-02 23:52:41 +0530

    changed some things

--- recipe_app/src/components/recipe-list/index.jsx
+++ recipe_app/src/components/recipe-list/index.jsx
@@ -1,4 +1,12 @@
 import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 const RecipeList = ({ recipes }) => {
   console.log(recipes);
@@ -8,6 +16,35 @@ const RecipeList = ({ recipes }) => {
       <Link href={"/"} className="text-2xl">
         Go Back
       </Link>
+      <div className="flex">
+        <Card>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
+      </div>
+
+      <div className=""></div>
     </div>
   );
 };
