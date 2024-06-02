b88862ff4b1cfa97ebfe78a7f7b73ca3c3c19e60
Pradhan-G <abhishekpradhan12.10.00@gmail.com>
2024-06-02 14:51:11 +0530

    Recipe App Setup

---
+++ recipe_app/src/app/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div className="w-full text-center">
+      <h1 className="text-5xl">Recipe App</h1>
+    </div>
+  );
+};
+
+export default LandingPage;
