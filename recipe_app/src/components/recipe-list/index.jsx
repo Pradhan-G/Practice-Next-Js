0ddf07a33443b58149fe165004c2938a88e1b20a
Pradhan-G <abhishekpradhan12.10.00@gmail.com>
2024-06-03 17:52:48 +0530

    content loaded and running

--- recipe_app/src/components/recipe-list/index.jsx
+++ recipe_app/src/components/recipe-list/index.jsx
@@ -7,6 +7,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import Image from "next/image";
 
 const RecipeList = ({ recipes }) => {
   console.log(recipes);
@@ -16,32 +17,27 @@ const RecipeList = ({ recipes }) => {
       <Link href={"/"} className="text-2xl">
         Go Back
       </Link>
-      <div className="flex">
-        <Card>
-          <CardHeader>
-            <CardTitle>Card Title</CardTitle>
-            <CardDescription>Card Description</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
-        </Card>
-
-        <Card>
-          <CardHeader>
-            <CardTitle>Card Title</CardTitle>
-            <CardDescription>Card Description</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
-        </Card>
+      <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
+        {recipes && recipes.length > 0
+          ? recipes.map((e, index) => (
+              <Card key={index}>
+                <CardHeader>
+                  <CardTitle>{e.name}</CardTitle>
+                  <CardDescription>Difficulty : {e.difficulty}</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <p>
+                    {e.instructions.map((elem, ind) => (
+                      <h1 key={ind}>{elem}</h1>
+                    ))}
+                  </p>
+                </CardContent>
+                <CardFooter className="min-w-full text-center">
+                  <p>{e.cuisine}</p>
+                </CardFooter>
+              </Card>
+            ))
+          : null}
       </div>
 
       <div className=""></div>
