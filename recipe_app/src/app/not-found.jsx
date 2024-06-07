b20f48432c8e5477ea4c9750a11b5723be45dca5
Pradhan-G <abhishekpradhan12.10.00@gmail.com>
2024-06-07 23:45:24 +0530

    updated Not Found

--- recipe_app/src/app/not-found.jsx
+++ recipe_app/src/app/not-found.jsx
@@ -1,5 +1,12 @@
 import Link from "next/link";
 
 export default function NotFound() {
-  return <h1>NotFound</h1>;
+  return (
+    <div className="w-full text-center">
+      <h1 className="text-5xl mb-10">This Page Does not Exist </h1>
+      <Link href={"/"} className="text-2xl">
+        Go Back
+      </Link>
+    </div>
+  );
 }
