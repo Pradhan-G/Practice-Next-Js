aedfc9390306a3ae98f942a9d83d3c98b99fbe8a
Pradhan-G <abhishekpradhan12.10.00@gmail.com>
2024-06-07 23:44:20 +0530

    removing things

--- recipe_app/src/app/not-found.jsx
+++ recipe_app/src/app/not-found.jsx
@@ -1,12 +1,5 @@
 import Link from "next/link";
 
 export default function NotFound() {
-  return (
-    <div className="w-full text-center">
-      <h1 className="text-5xl mb-10">This Page Does not Exist </h1>
-      <Link href={"/"} className="text-2xl">
-        Go Back
-      </Link>
-    </div>
-  );
+  return <h1>NotFound</h1>;
 }
--- recipe_app/src/app/page.jsx
+++ recipe_app/src/app/page.jsx
@@ -2,17 +2,7 @@ import Link from "next/link";
 import React from "react";
 
 const LandingPage = () => {
-  return (
-    <div className="w-full h-[100vh] bg-slate-400 text-center flex flex-col items-center justify-center ">
-      <h1 className="text-5xl mb-10 text-white">Recipe App</h1>
-      <Link
-        href={"/recipe-list"}
-        className=" rounded bg-slate-700 hover:bg-slate-900 text-white py-4 px-3  text-xl font-extrabold "
-      >
-        Explore
-      </Link>
-    </div>
-  );
+  return <h1>Landing Page</h1>;
 };
 
 export default LandingPage;
